40d01e79f00f52b93b3b3231f3a690692246de95
Fitsum A <fitsum@gmail.com>
2021-04-12 02:07:22 +0300

    testing passing data to html

--- covidapp/views.py
+++ covidapp/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def helloWorld(request):
-    return render(request,'hello.html')
+    context = {'greeting': 'Everyone !!'}
+    return render(request,'hello.html',context)
