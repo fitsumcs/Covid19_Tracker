f5f257a6b1d1b6568bcee0a6171962791be78dc6
Fitsum A <fitsum@gmail.com>
2021-04-12 10:31:12 +0300

    display Json Data

--- covidapp/views.py
+++ covidapp/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+import requests
+import json
+
+url = "https://covid-193.p.rapidapi.com/statistics"
+
+headers = {
+    'x-rapidapi-key': "64bca0886bmsha7377930000aed5p1a6aecjsn8fa7b68dbbfb",
+    'x-rapidapi-host': "covid-193.p.rapidapi.com"
+    }
+
+response = requests.request("GET", url, headers=headers).json()
 
 # Create your views here.
 def helloWorld(request):
-    context = {'greeting': 'Everyone !!'}
+    context = {'greeting': response}
     return render(request,'hello.html',context)
